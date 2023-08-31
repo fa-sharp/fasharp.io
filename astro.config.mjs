f39c3af35e439305100a14e2c618b5c278037255
fa-sharp <farshadkt@gmail.com>
2023-08-31 00:27:33 -0400

    🚀 upgrade to astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true
-  },
   integrations: [preact()]
 });
