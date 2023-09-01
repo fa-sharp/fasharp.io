0b7a1121c85f26666b8bda8637f3a7ab868feff7
fa-sharp <farshadkt@gmail.com>
2023-09-01 03:17:24 -0400

    new post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,28 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
-import icon from 'astro-icon'
+import embeds from "astro-embed/integration";
+
+import remarkGFM from 'remark-gfm'
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), icon({
-    include: {
-      'fa6-regular': ['*'],
-      'fa6-brands': ['*']
-    }
-  })]
+  markdown: {
+    remarkPlugins: [remarkGFM],
+    rehypePlugins: [rehypeAccessibleEmojis]
+  },
+  integrations: [
+    preact(),
+    embeds(),
+    icon({
+      include: {
+        "fa6-regular": ["*"],
+        "fa6-brands": ["*"],
+      },
+    }),
+    mdx(),
+  ],
 });
