f31211b97e131b37379d2f9bfd6bd847162da0bc
fa-sharp <farshadkt@gmail.com>
2023-09-03 06:20:35 -0400

    styling stuff, first keyboard a11y post

---
+++ src/components/demos/a11y/Buttons.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "preact/hooks";
+import styles from "./Buttons.module.css";
+
+export function DivButtons() {
+  const [inView, setInView] = useState(0);
+
+  return !inView ? (
+    <div className={styles.container}>
+      {[1, 2, 3, 4, 5, 6].map((num) => (
+        <div className={styles.button} onClick={() => setInView(num)}>
+          #{num}
+        </div>
+      ))}
+    </div>
+  ) : (
+    <div className={styles.container}>
+      <div className={styles.button} onClick={() => setInView(0)}>
+        #{inView} details (click to return)
+      </div>
+    </div>
+  );
+}
+
+export function PlainButtons() {
+  const [inView, setInView] = useState(0);
+
+  return !inView ? (
+    <div className={styles.container}>
+      {[1, 2, 3, 4, 5, 6].map((num) => (
+        <button className={styles.button} onClick={() => setInView(num)}>
+          #{num}
+        </button>
+      ))}
+    </div>
+  ) : (
+    <div className={styles.container}>
+      <button className={styles.button} onClick={() => setInView(0)}>
+        #{inView} details (click to return)
+      </button>
+    </div>
+  );
+}
