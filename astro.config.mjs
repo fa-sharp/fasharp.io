49aab0bc1d41c83201d30f2720f7a481eebe859b
fa-sharp <farshadkt@gmail.com>
2024-06-09 21:07:02 -0400

    deploy to fly.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://fasharp-io.fly.dev",
   markdown: {
     remarkPlugins: [remarkGFM],
     rehypePlugins: [rehypeAccessibleEmojis]
