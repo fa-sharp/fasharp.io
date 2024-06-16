4f7b6e1614d15ef04f1c853753469d8f43cd7c1f
fa-sharp <farshadkt@gmail.com>
2024-06-15 22:18:08 -0400

    add rate limit

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,12 @@
-import { defineAction, z } from "astro:actions";
+import { ActionError, defineAction, z } from "astro:actions";
 import { getEntry } from "astro:content";
+import { RateLimiterMemory } from "rate-limiter-flexible";
 import { addLike, getPostData, removeLike } from "src/database";
+
+const rateLimiter = new RateLimiterMemory({
+  points: 100,
+  duration: 60,
+});
 
 export const server = {
   like: defineAction({
@@ -13,7 +19,15 @@ export const server = {
           "Post not found!"
         ),
     }),
-    async handler({ like, postSlug }) {
+    async handler({ like, postSlug }, ctx) {
+      try {
+        await rateLimiter.consume(ctx.clientAddress);
+      } catch {
+        throw new ActionError({
+          message: "Slow down!",
+          code: "TOO_MANY_REQUESTS",
+        });
+      }
       if (like) {
         return { likes: await addLike(postSlug) };
       } else {
